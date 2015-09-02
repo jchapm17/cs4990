48062d444b9efc9c2b7c986563b63a347017b3b7
jchapm17 <D00266036@dmail.dixie.edu>
2015-09-02 16:58:39 -0600

    third commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,7 +8,6 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 
 from .models import Choice,  Question
-# Create your views here.
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -31,7 +30,6 @@ def vote(request, question_id):
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': p,
             'error_message': "You didn't select a choice.",
@@ -39,7 +37,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
