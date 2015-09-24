9be3095c56eb658d365912ad2a69d08a13b956b7
Jamie Chapman <jchapman@nitrogen.cs.dixie.edu>
2015-09-23 23:48:56 -0600

    comment submission

--- blog/blogapp/forms.py
+++ blog/blogapp/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 
-from .models import Post
-
-class PostForm(forms.ModelForm):
-
-    class Meta:
-        model = Post
-        fields = ('title', 'text',)
+class CommentForm(forms.Form):
+	name = forms.CharField(max_length=200, label='Name')
+	comment = forms.CharField(widget=forms.Textarea, max_length=1000, label='Comment')
+	post_id = forms.IntegerField(widget=forms.HiddenInput)
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.core.context_processors import csrf
+#from django.core.context_processors import csrf
+from django.core.urlresolvers import reverse_lazy
 
 from .models import Post, Comment, Category
+from .forms import CommentForm
+
 # Create your views here.
 class IndexView(generic.ListView):
 	template_name = 'blogapp/index.html'
@@ -14,6 +17,25 @@ class IndexView(generic.ListView):
 
 class PostDetailView(generic.DetailView):
 	model = Post	
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(PostDetailView,self).get_context_data()
+		context["form"] = CommentForm(initial={'post_id': self.object.pk})
+		return context;
+
+class PostCommentFormView(generic.detail.SingleObjectMixin, generic.FormView):
+	form_class = CommentForm
+	model = Post
+	success_url = reverse_lazy('blogapp:comment_success')
+
+	def form_valid(self, form):
+		
+		comment = Comment()
+		comment.post = get_object_or_404(Post, pk=form.cleaned_data["post_id"])
+		comment.person = form.cleaned_data["name"]
+		comment.comment_text = form.cleaned_data["comment"]
+		comment.save()
+		return super(PostCommentFormView, self).form_valid(form)
 
 class CategoryDetailView(generic.DetailView):
 	model = Category
