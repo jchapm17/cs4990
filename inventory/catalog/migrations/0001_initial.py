ea22035e205cd552c18cb372e05cc32eed257b98
Jamie Chapman <jchapman@selenium.cs.dixie.edu>
2015-09-25 17:38:11 -0600

    inventory caught up

---
+++ inventory/catalog/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('descriptiom', models.TextField(null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('quantity', models.IntegerField(default=0)),
+                ('sku', models.CharField(max_length=200, null=True, blank=True)),
+                ('category', models.ForeignKey(to='catalog.Category')),
+            ],
+        ),
+    ]
